d20658178a04498aaa0aa6b8b53fed7a9f9a2fa8
JackyNg2002 <kkll22kkll22kkll22@gmail.com>
2024-03-24 23:15:05 +0800

    yolo8

---
+++ y8-segment.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8n-seg.pt')  # load an official model
+
+
+# Predict with the model
+results = model('https://ultralytics.com/images/bus.jpg', save=True)  # predict on an image
